2e9a3457b2eca3229551cf80acb5ac6c07045679
suu3 <71166763+suu3@users.noreply.github.com>
2021-02-26 04:28:10 +0900

    create new project

---
+++ EliminateGhost/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    Material mat;
+    float currentXoffset = 0;
+    float speed = 0.08f;
+
+    void Start()
+    {
+        mat = GetComponent<SpriteRenderer>().material;        
+    }
+
+    void Update()
+    {
+        currentXoffset += speed * Time.deltaTime;
+        mat.mainTextureOffset = new Vector2(currentXoffset, 0);
+    }
+}
---
+++ EliminateGhost/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    
+    Transform tr;
+    float limitPointMin = -6.02f;
+    float limitPointMax = 6.02f;
+
+    //create weapone
+    public GameObject prefabWeapon;
+    
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        tr.position = new Vector2(-5.74f, -1.88f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //좌우 키보드 사용
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tr.position = new Vector2(tr.position.x + 0.01f * speed, tr.position.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tr.position = new Vector2(tr.position.x - 0.01f * speed, tr.position.y);
+        }
+
+        //limit
+        if(tr.position.x < limitPointMin)
+        {
+            tr.position = new Vector2(limitPointMin, tr.position.y);
+        }
+
+        if (tr.position.x > limitPointMax)
+        {
+            tr.position = new Vector2(limitPointMax, tr.position.y);
+        }
+
+        // Weapon 생성
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(prefabWeapon, new Vector2(tr.position.x + 1.65f, tr.position.y + 0.66f), Quaternion.identity);
+
+        }
+
+    }
+}
