98219d79ae68599839a1da12acc85f74bc1484a1
suu3 <71166763+suu3@users.noreply.github.com>
2021-02-26 06:13:46 +0900

    create enemy

---
+++ EliminateGhost/Assets/Scripts/EnemyCreate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreate : MonoBehaviour
+{
+    public GameObject prefabEnemy;
+
+    public Vector2 limitMax;
+    public Vector2 limitMin;
+
+    void Start()
+    {
+        StartCoroutine(CreateEnemy());      
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CreateEnemy()
+    {
+        while (true)
+        {
+            float locationX = Random.Range(limitMin.x, limitMax.x);
+            float locationY = Random.Range(limitMin.y, limitMax.y);
+            Vector2 creatingPoint = new Vector2(locationX, locationY);
+
+            Instantiate(prefabEnemy, creatingPoint, Quaternion.identity);
+
+            float creatingTime = Random.Range(0.5f, 3.0f);
+            yield return new WaitForSeconds(creatingTime);
+
+        }
+    }
+}
---
+++ EliminateGhost/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    Transform tr;
+    public float speed;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        StartCoroutine(DestroySelf());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.Translate(Vector2.left * speed);   
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(10.0f);
+        Destroy(this.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+        Destroy(collision.gameObject);
+    }
+}
